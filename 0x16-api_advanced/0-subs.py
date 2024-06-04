ec8327b85c8a9ec9d07b48a16ee1950580370704
Samuel Amenyeawu <80473557+delaworla@users.noreply.github.com>
2024-06-04 22:37:48 +0000

    defined functions

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,4 +4,7 @@
 A function that queries the Reddit API and returns the number of subscribers 
 (not active users, total subscribers) for a given subreddit. 
 If an invalid subreddit is given, the function should return 0.
+
 """
+
+def number_of_reddit_subscribers(subreddit):
